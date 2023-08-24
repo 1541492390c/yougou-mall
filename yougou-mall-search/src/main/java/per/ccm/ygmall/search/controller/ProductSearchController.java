1f93031faf1e57bf500b259a16de575f543308e9
15414 <1541492390@qq.com>
2023-08-24 20:54:17 +0800

    修改

--- yougou-mall-search/src/main/java/per/ccm/ygmall/search/controller/ProductSearchController.java
+++ yougou-mall-search/src/main/java/per/ccm/ygmall/search/controller/ProductSearchController.java
@@ -1,6 +1,8 @@
 package per.ccm.ygmall.search.controller;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +23,15 @@ public class ProductSearchController {
 
     @GetMapping("/keyword")
     @Operation(summary = "关键词搜索", description = "传入商品名称、排序方式、页数、当前页进行关键词搜索")
+    @Parameters({
+            @Parameter(name = "keyword", description = "关键词"),
+            @Parameter(name = "category_node", description = "分类节点"),
+            @Parameter(name = "brand_id", description = "品牌ID"),
+            @Parameter(name = "sort", description = "排序字段"),
+            @Parameter(name = "order", description = "排序方式 asc-正序 desc-倒序"),
+            @Parameter(name = "page_num", description = "当前页"),
+            @Parameter(name = "page_size", description = "页数")
+    })
     public ResponseEntity<ESProductResultVO> keywordSearch(
             @RequestParam(value = "keyword", required = false) String keyword,
             @RequestParam(value = "category_node", required = false) String categoryNode,
